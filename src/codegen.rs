f61de7ecbb6ac17e9ee2866034ec19c7b40c15d2
jrasmusbm <JRasmusBm@gmail.com>
2023-06-10 15:53:08 +0200

    feat: add wrapped AST kind

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,15 +1,23 @@
 pub enum AstKind {
     Literal,
+    Wrapped,
 }
 
 pub struct AstNode {
     pub kind: AstKind,
     pub format: String,
+    pub children: Vec<AstNode>,
 }
 
 pub fn generate(node: &AstNode) -> Option<String> {
     match node.kind {
         AstKind::Literal => return Some(node.format.clone()),
+        AstKind::Wrapped => {
+            if let Some(child) = generate(&node.children[0]) {
+                return Some(node.format.replace("{:1}", &child));
+            }
+            return None;
+        }
     }
 }
 
@@ -22,8 +30,26 @@ mod tests {
         let node = AstNode {
             kind: AstKind::Literal,
             format: String::from("hello"),
+            children: vec![],
         };
         let want = String::from("hello");
+        let got = generate(&node).unwrap_or(String::from("NOT IMPLEMENTED!"));
+
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn can_print_wrapped_identifiers() {
+        let node = AstNode {
+            kind: AstKind::Wrapped,
+            format: String::from("'{:1}'"),
+            children: vec![AstNode {
+                kind: AstKind::Literal,
+                format: String::from("hello"),
+                children: vec![],
+            }],
+        };
+        let want = String::from("'hello'");
         let got = generate(&node).unwrap_or(String::from("NOT IMPLEMENTED!"));
 
         assert_eq!(want, got);
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     if let Some(result) = codegen::generate(&codegen::AstNode {
         kind: codegen::AstKind::Literal,
         format: String::from("hello"),
+        children: vec![],
     }) {
         println!("{}", result);
     } else {
