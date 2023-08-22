307e0f2fe9a6f641d64cac393cb1c9cf676cb3ea
jrasmusbm <JRasmusBm@gmail.com>
2023-08-22 20:09:55 +0200

    test: error on layout mismatch

--- pkg/ast/composite_test.go
+++ pkg/ast/composite_test.go
@@ -6,16 +6,52 @@ import (
 	"testing"
 )
 
+func assertStringsMatch(t testing.TB, got, want string, err error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	if err != nil {
+
+		t.Errorf("Expected no errors, got `%s`", err.Error())
+	}
+}
+
+func assertError(t testing.TB, err error, want string) {
+	t.Helper()
+
+	if err == nil {
+		t.Errorf("Expected error: `%s` got nil.", want)
+	}
+
+	got := err.Error()
+	if got != want {
+		t.Errorf("Expected error: `%s` got `%s`.", got, want)
+	}
+}
+
 func TestCompositeCodeString(t *testing.T) {
 	t.Run("Applies formatting to the children", func(t *testing.T) {
 
-		got, _ := ast.NewComposite(fmt.Sprintf("(%s)", ast.PLACEHOLDER), []ast.Node{
+		got, err := ast.NewComposite(fmt.Sprintf("(%s)", ast.PLACEHOLDER), []ast.Node{
 			ast.NewLiteral("hello"),
 		}).CodeString()
 		want := "(hello)"
 
-		if got != want {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertStringsMatch(t, got, want, err)
+	})
+
+	t.Run("Returns error on placeholder mismatch", func(t *testing.T) {
+		_, err := ast.NewComposite(
+			fmt.Sprintf("(%s, %s, %s)", ast.PLACEHOLDER, ast.PLACEHOLDER, ast.PLACEHOLDER),
+			[]ast.Node{
+				ast.NewLiteral("hello"),
+				ast.NewLiteral("world"),
+			},
+		).CodeString()
+
+		assertError(t, err, "Layout mismatch, 3 placeholders and 2 children.")
 	})
 }
